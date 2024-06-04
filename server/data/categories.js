8059f5ce3d03696d049ee611ee27d49622fc603b
khanhnguyentuan <khanh.nguyentuan123@hcmut.edu.vn>
2024-06-04 16:11:46 +0700

    Refactor category names and descriptions

--- server/data/categories.js
+++ server/data/categories.js
@@ -1,22 +1,22 @@
 const categories = [
     {
-        categoryName: "Giày thể thao",
-        description: "Giày thể thao nhiều thể loại giúp các cánh đàn ông đá người yêu đẹp như ronaldô ghi bàn :D",
+        categoryName: "Món ăn ba miền",
+        description: "Món ăn ba miền Việt Nam",
         thumb: "https://allimages.sgp1.digitaloceanspaces.com/pedrovietnamcom/2021/06/Cac-doi-giay-da-bong-nao-dang-hot-trend-nam.jpg",
     },
     {
-        categoryName: "Giày thời trang",
-        description: "Nói chung là đẹp",
+        categoryName: "Món ăn đường phố",
+        description: "Món ăn đường phố Việt Nam",
         thumb: "https://www.thol.com.vn/pub/media/catalog/product/cache/image/800x800/e9c3970ab036de70892d86c6d221abfe/g/y/gym-shoes-03.jpg",
     },
     {
-        categoryName: "Giày da nam",
-        description: "Giày da nam",
+        categoryName: "Món ăn cho mẹ và bé",
+        description: "Món ăn cho mẹ và bé",
         thumb: "https://1.bp.blogspot.com/-YMoRGnqKowQ/X0ymbrlgKwI/AAAAAAAAH5A/chcmEheaS5gjj5vt2C5FwS3dJLMLRnLCACLcBGAsYHQ/s1600/gi%25C3%25A0y%2Bl%25C6%25B0%25E1%25BB%259Di4.jpg",
     },
     {
-        categoryName: "Giày da nữ",
-        description: "Giày da nữ",
+        categoryName: "Pha chế tổng hợp",
+        description: "Pha chế tổng hợp",
         thumb: "https://salt.tikicdn.com/cache/w1200/ts/product/47/7d/36/023fb0813402e1acbf22e322f23d7cb5.jpg",
     },
 ];
