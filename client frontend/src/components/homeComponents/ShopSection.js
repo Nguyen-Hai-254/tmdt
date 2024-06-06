d437eba86a534c7196c8036c4d0742e211c0c86f
khanhnguyentuan <khanh.nguyentuan123@hcmut.edu.vn>
2024-06-06 20:41:05 +0700

    feat: fix api

--- client frontend/src/components/homeComponents/ShopSection.js
+++ client frontend/src/components/homeComponents/ShopSection.js
@@ -1,72 +1,85 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Pagination from "./pagination";
-import { useDispatch, useSelector } from "react-redux";
-import { listProduct } from "../../Redux/Actions/ProductActions";
 import Loading from "../LoadingError/Loading";
 import Message from "../LoadingError/Error";
 import { NavBtnLink } from "../Navbar/NavElement";
+import { convertToBase64 } from "../utils/convert";
+import axios from 'axios';
 
 const ShopSection = (props) => {
   const { keyword, pagenumber } = props;
-  const dispatch = useDispatch();
+  const [courseList, setCourseList] = useState([]);
 
-  const productList = useSelector((state) => state.productList);
-  const { loading, error, products, page, pages } = productList;
+  const getCourseListByCategory = useCallback(async (category) => {
+    try {
+      const response = await axios.get(`/api/courses/get-course-by-category/${category}`);
+      return response.data; // Trả về dữ liệu thay vì gán vào state
+    } catch (error) {
+      console.error('Failed to get course list by category', error);
+    }
+  }, []);
 
   useEffect(() => {
-    dispatch(listProduct(keyword, pagenumber));
-  }, [dispatch, keyword, pagenumber]);
+    const categories = ['monngonbamien', 'category2', 'category3']; // Danh sách các category
+
+    Promise.all(categories.map(category => getCourseListByCategory(category)))
+      .then(results => {
+        // results là một mảng chứa kết quả của tất cả các Promise
+        // Mỗi phần tử trong results tương ứng với kết quả của một Promise
+        // Gộp tất cả các kết quả lại thành một mảng duy nhất và gán vào state
+        setCourseList(results.flat());
+      });
+  }, [getCourseListByCategory]);
   return (
     <>
+      
       <div className="container">
         <div className="section">
           <div className="row">
             <div className="col-lg-12 col-md-12 article">
               <div className="shopcontainer row">
-                {loading ? (
-                  <div className="mb-5">
-                    <Loading />
-                  </div>
-                ) : error ? (
-                  <Message variant="alert-danger">{error}</Message>
-                ) : (
+                {
+                  courseList.length === 0 ? (
+                    <div className="mb-5">
+                      <Loading />
+                    </div>
+                  ) : 
+                 (
                   <>
-                    {products.map((product) => (
+                    
+                    {courseList.map((course) => (
 
                       <div
                         className="shop col-lg-4 col-md-6 col-sm-6"
-                        key={product._id}
+                        key={course._id}
                       >
                         <div className="border-product">
-                          <Link to={`/products/${product._id}`}>
+                          <Link to={`/products/${course._id}`}>
                             <div className="shopBack">
-                              <img src={product.image} alt={product.name} />
+                              <img src={course.image} alt={course.name} />
                             </div>
                           </Link>
 
                           <div className="shoptext">
                             <p>
-                              <Link to={`/products/${product._id}`}>
-                                {product.name}
+                              <Link to={`/products/${course._id}`}>
+                                {course.name}
                               </Link>
 
                             </p>
-                            {product.discountID !== "" ? (
                               <p>
                                 <NavBtnLink to={''}>
-                                  Đang hạ giá
+                                  Time
                                 </NavBtnLink>
                               </p>
-                              
-                            ): <p></p>}
-                            
                             <Rating
                               value={product.rating}
                               text={`${product.numReviews} Đánh giá`}
                             />
-                            <h3>{product.price}Đ</h3>
+                            <h3>{course.price}Đ</h3>
+                            {/* <h3>{course.description}Đ</h3> */}
                           </div>
                         </div>
                       </div>
